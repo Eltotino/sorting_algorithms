781e2cf4ad56b1d0a3a7320cd7b6ca8017f46025
Eltotino <totojaber1@gmail.com>
2021-07-05 21:09:26 +0100

    shell

---
+++ 100-shell_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+* shell_sort - sorts array
+* @array: array
+* @size: size
+*
+* Return: void
+*/
+void shell_sort(int *array, size_t size)
+{
+	size_t a, b;
+	size_t btwn = 1;
+	int insert;
+
+	if (!array || !size)
+		return;
+	while (btwn < size / 3)
+		btwn = btwn * 3 + 1;
+	while (btwn > 0)
+	{
+		for (a = btwn; a < size; a++)
+		{
+			insert  = array[a];
+			b = a;
+			while (b > (btwn - 1) && array[b - btwn] >= insert)
+			{
+				array[b] = array[b - btwn];
+				b -= btwn;
+			}
+			array[b] = insert;
+		}
+		btwn /= 3;
+		print_array(array, size);
+	}
+}
--- sort.h
+++ sort.h
@@ -30,4 +30,5 @@ void Lumoto_sort(int *array, int low, int high, size_t size);
 int partition(int *array, int low, int high, size_t size);
 void change(int *one, int *two);
 void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 #endif
